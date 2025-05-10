4c4229fbb03bdcec7d8a18e87801104a738fea18
evileg0 <30571299+evileg0@users.noreply.github.com>
2025-05-10 13:14:57 +0300

    working login

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def HomePage(request):
+    logedusername = "Unknown user"
+    if request.user.is_authenticated:
+        logedusername = request.user.username
+    return render(request, 'home.html', {'username': logedusername})
